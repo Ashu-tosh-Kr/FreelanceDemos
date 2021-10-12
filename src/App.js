76b49ab70bd4f2cf6cd307f52307e1e5bb932d14
Ashu-tosh-Kr <64960336+Ashu-tosh-Kr@users.noreply.github.com>
2021-10-12 15:47:10 +0530

    absolute imports and initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import Homepage from './pages/Homepage';
+import { Route, Switch } from 'react-router-dom';
+
+import theme from './theme';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <ChakraProvider theme={theme}></ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Homepage} />
+      </Switch>
+    </ChakraProvider>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <StrictMode>
     <ColorModeScript />
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
   document.getElementById('root')
 );
